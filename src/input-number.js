e8be6a34a6cf6df178fd8b7f8d43e1ee0ba3d7e9
tianxiangbing <55342775@qq.com>
2021-05-28 15:00:38 +0800

    新增formatThousand:true 千分位配置

--- src/input-number.js
+++ src/input-number.js
@@ -8,46 +8,93 @@
  * Contact: 55342775@qq.com
  */
 ;
-(function(root, factory) {
+(function (root, factory) {
     //amd
     if (typeof exports === 'object') { //umd
         module.exports = factory();
     } else {
         root.InputNumber = factory(window.Zepto || window.jQuery || $);
     }
-})(this, function() {
+})(this, function () {
     //添加xtype为InputNumber
-    if(window.Eui){
+    if (window.Eui) {
         Eui.Extra.registerUiExtType('InputNumber', {
             options: {
-                decimal:2
+                decimal: 2
             },
             handler(target, options) {
-                var $target = Eui.getJqDom(target);
+                var $target = $(target);
                 $target.InputNumber(options);
             }
         });
     }
-    $.fn.InputNumber = function(settings) {
+    $.fn.InputNumber = function (settings) {
         var arr = [];
-        $(this).each(function() {
-            if($(this).data('component')){
+        $(this).each(function () {
+            if ($(this).data('component')) {
                 $(this).data('component').update(settings);
-            }else{
+            } else {
                 var options = $.extend({
                     trigger: $(this)
                 }, settings);
                 var number = new InputNumber();
                 number.init(options);
-                $(this).data('component',number);
+                $(this).data('component', number);
                 arr.push(number);
             }
         });
         return arr;
     };
-    var InputNumber = function() {};
+
+    /**
+     *   说明: 设置文本框的光标位置
+     *   参数: 文本框对象 {dom object}, 光标的位置 {int}
+     * 返回值: {void}
+     */
+    function setTxtCursorPosition(txtObj, pos) {
+        var tempObj = txtObj;
+        var cursurPosition = -1;
+        if (tempObj.selectionStart != undefined) { //非IE浏览器
+            tempObj.setSelectionRange(pos, pos);
+        } else { //IE
+            var range = tempObj.createTextRange();
+            range.move("character", pos);
+            range.select();
+        }
+    }
+    //获取当前光标位置
+    const getPosition = function (element) {
+        var cursorPos = 0;
+        if (document.selection) {//IE
+            var selectRange = document.selection.createRange();
+            selectRange.moveStart('character', -element.value.length);
+            cursorPos = selectRange.text.length;
+        } else if (element && (element.selectionStart || element.selectionStart == '0')) {
+            cursorPos = element.selectionStart;
+        }
+        return cursorPos;
+    }
+    function getInputSelection(element) {
+        var myArea = element;
+        var selection;
+        if (myArea.selectionStart) {
+            if (myArea.selectionStart != undefined) {
+                selection = myArea.value.substr(myArea.selectionStart, myArea.selectionEnd - myArea.selectionStart);
+            }
+        } else {
+            if (window.getSelection) {  //非ie浏览器,firefox,safari,opera
+                selection = window.getSelection();
+            } else if (document.getSelection) {
+                selection = document.getSelection();
+            } else if (document.selection) {
+                selection = document.selection.createRange().text;
+            }
+        }
+        return selection;
+    }
+    var InputNumber = function () { };
     InputNumber.prototype = {
-        init: function(settings) {
+        init: function (settings) {
             var _this = this,
                 minus = ''
             settings = $.extend({
@@ -55,7 +102,7 @@
                 decimal: 2,
                 minus: false, //是否支持负数,默认不支持
                 parent: 'body',
-                maxLength:30
+                maxLength: 30
             }, settings);
             this.settings = settings;
             var excludeKey = {
@@ -79,7 +126,7 @@
                 regex = '([1-9]\\d*(\\.\\d{1,' + settings.decimal + '})?|([^0]{1}(\\.\\d{1,' + settings.decimal + '}))?)';
             }
             parent = parent || 'body';
-            $(settings.trigger, parent).each(function(i, v) {
+            $(settings.trigger, parent).each(function (i, v) {
                 var txt, formatTxt;
                 if ($(v)[0].tagName == 'INPUT') {
                     txt = $(v).val();
@@ -99,78 +146,100 @@
                 }
             });
 
-            $(settings.trigger, parent).on('keyup', function(e) {
-                let value = e.target.value;
-                e.target.value = value.replace(/[^\d,,\.\-]]*/g,'');
+            $(settings.trigger, parent).on('keyup', function (e) {
+                var exe = /^(\-\-)+/g;
+                var value = e.target.value;
+                if(exe.test(value)){
+                    value=$(this).val().replace(exe,'-')
+                    $(this).val(value);
+                    setTxtCursorPosition(e.target,1);
+                }
+                // console.log('up:::', value)
+                var intN = value.replace(/\-/g,'').split('.')[0];
+                if (intN.length > settings.maxLength) {
+                    value = $(this).data('prev');
+                    $(this).val(value);
+                    setTxtCursorPosition(e.target, $(this).data('prevPos'));
+                }
+                e.target.value = value.replace(/[^\d,,\.\-]]*/g, '');
                 checkNumber.call(this, e)
             });
-            //获取当前光标位置
-            const getPosition = function (element) {
-                let cursorPos = 0;
-                if (document.selection) {//IE
-                    var selectRange = document.selection.createRange();
-                    selectRange.moveStart('character', -element.value.length);
-                    cursorPos = selectRange.text.length;
-                } else if (element && (element.selectionStart || element.selectionStart == '0')) {
-                    cursorPos = element.selectionStart;
+            $(settings.trigger, parent).on('keydown', function (e) {
+                var pos = getPosition(e.target);
+                var txt = getInputSelection(e.target);
+                var v = String($(this).val());
+                // console.log('down:::', v)
+                $(this).data('prev', v);
+                $(this).data('prevPos', pos);
+                // console.log(pos, txt, v)
+                var left = v.substring(0, pos);
+                var right = v.substring(txt.length + pos);
+                var valueArray = $(this).val().replace(/\,/gi, '').split('.');
+                //实现K\M
+                if (e.keyCode === 75 && valueArray != '0' && $(this).val() != '') {
+                    //K
+                    // valueArray[0] += '000';
+                    // var value = valueArray.length == 2 ? valueArray[0] + '.' + valueArray[1] : valueArray[0];
+                    var value = left + '000' + right;
+                    $(this).val(value);
+                    setTxtCursorPosition(e.target, pos+3);
+                    checkNumber.call(this, e)
+                    return false;
                 }
-                return cursorPos;
-            }
-            $(settings.trigger, parent).on('keydown', function(e) {
+                if (e.keyCode === 77 && valueArray != '0' && $(this).val() != '') {
+                    //M
+                    // valueArray[0] += '000000';
+                    // var value = valueArray.length == 2 ? valueArray[0] + '.' + valueArray[1] : valueArray[0];
+                    var value = left + '000000' + right;
+                    $(this).val(value);
+                    setTxtCursorPosition(e.target, pos+6);
+                    checkNumber.call(this, e)
+                    return false;
+                }
+                checkNumber.call(this, e)
+            });
+            $(settings.trigger, parent).on('keydown', function (e) {
                 // console.log(e.keyCode)
                 // console.log(e)
-                var valueArray = $(this).val().replace(/\,/gi,'').split('.');
-                //实现K\M
-                if(e.keyCode === 75 && valueArray!='0'){
-                    //K
-                    valueArray[0] += '000';
-                    var value = valueArray.length ==2 ? valueArray[0]+'.'+valueArray[1]:valueArray[0];
-                    $(this).val(value);
-                    checkNumber.call(this, e)
-                    return false;
-                }
-                if(e.keyCode === 77 && valueArray!='0'){
-                    //M
-                    valueArray[0] += '000000';
-                    var value = valueArray.length ==2 ? valueArray[0]+'.'+valueArray[1]:valueArray[0];
-                    $(this).val(value);
-                    checkNumber.call(this, e)
-                    return false;
-                }
-                if(e.keyCode == 8){
+                if (e.keyCode == 8) {
                     return;
                 }
                 var pos = getPosition(e.target);
                 // console.log(pos)
-                if(e.key=='-' && pos == 0 && settings.minus ){
+                if (e.key == '-' && pos == 0 && settings.minus) {
                     return;
                 }
-                if(e.key =='.' && pos==$(this).val().length && !isNaN($(this).val()) && $(this).val().indexOf('.')==-1){
+                if (e.key == '.' && pos == $(this).val().length && !isNaN($(this).val()) && $(this).val().indexOf('.') == -1) {
                     return;
                 }
-                if(isNaN(e.key) ){
+                if (isNaN(e.key) && ["ArrowLeft","ArrowRight","Delete"].indexOf(e.key)==-1) {
                     return false;
                 }
                 checkNumber.call(this, e)
             });
-            $(settings.trigger, parent).on('focus', function(e) {
-                var formatTxt = e.target.value.replace(/\,/g,'');
+            $(settings.trigger, parent).on('focus', function (e) {
+                var formatTxt = e.target.value.replace(/\,/g, '');
                 $(this).val(formatTxt);
             })
-            $(settings.trigger, parent).on('blur', function(e) {
-                let value = e.target.value;
-                e.target.value = value.replace(/[^\d,,\.\-]]*/g,'');
+            $(settings.trigger, parent).on('blur', function (e) {
+                var value = e.target.value;
+                e.target.value = value.replace(/[^\d,,\.\-]]*/g, '');
                 checkNumber.call(this, e)
                 var formatTxt = _this.doFormat(e.target.value);
                 $(this).val(formatTxt);
-                if($(this).val()=='-'){
+                var v = $(this).val()
+                var exe = /(^[0\,\.\-]+$)/
+                if ($(this).val() == '-') {
                     $(this).val('');
                     $(this).attr('data-value', '');
+                } else if (exe.test(v)) {
+                    $(this).val('0');
+                    $(this).attr('data-value', '0');
                 }
-                let tempValue = $(this).val();
+                var tempValue = $(this).val();
                 if (tempValue.lastIndexOf(".") >= 0) {
                     if (tempValue.lastIndexOf(".") == tempValue.length - 1) {
-                        $(this).val(tempValue.substr(0,tempValue.length-1));
+                        $(this).val(tempValue.substr(0, tempValue.length - 1));
                     }
                 }
             });
@@ -181,6 +250,7 @@
                 // GetNumberResult(e, $(this)[0], regex);
                 var v = _this.getMoneyfloat($(this).val());
                 $('#' + id).val(v);
+                $(this).val(v);
                 $(this).attr('data-value', v);
             }
 
@@ -296,54 +366,40 @@
                 return tempValue;
             }
 
-            /**
-             *   说明: 设置文本框的光标位置
-             *   参数: 文本框对象 {dom object}, 光标的位置 {int}
-             * 返回值: {void}
-             */
-            function setTxtCursorPosition(txtObj, pos) {
-
-                var tempObj = txtObj;
-                var cursurPosition = -1;
-
-                if (tempObj.selectionStart != undefined) { //非IE浏览器
-                    tempObj.setSelectionRange(pos, pos);
-                } else { //IE
-                    var range = tempObj.createTextRange();
-                    range.move("character", pos);
-                    range.select();
-                }
-            }
         },
-        update(settings){
+        update(settings) {
             $.extend(this.settings, settings);
         },
-        getMoneyfloat: function(s) {
+        getMoneyfloat: function (s) {
             if (s == '') {
                 return null;
             }
-            return (s + "").replace(/[^\d\.-]/g, "");
+            return (s + "").replace(/[^\d\.-]/g, "").replace(/^[0]+/, '0').replace(/^\-[0]+/, '-0');
         },
-        doFormat: function(s) {
+        doFormat: function (s) {
             var _this = this;
             if (!s) return "";
 
             if ($.isNumeric(s)) {
                 s = s.toString();
             }
-            if(_this.settings.decimal >0 && _this.settings.isAutoZero && !isNaN(s)){
+            if (_this.settings.decimal > 0 && _this.settings.isAutoZero && !isNaN(s)) {
                 var arr = s.split('.');
-                var decimals= _this.settings.decimal;
-                if(arr.length>1){
-                    var d = arr.length ==1?0:arr[1].length;
+                var decimals = _this.settings.decimal;
+                if (arr.length > 1) {
+                    var d = arr.length == 1 ? 0 : arr[1].length;
                     s += Array(decimals + 1).join(0).slice(0, Math.max(0, decimals - d));
-                }else{
-                    s += '.'+ Array(decimals + 1).join(0).slice(0, Math.max(0, decimals ));
+                } else {
+                    s += '.' + Array(decimals + 1).join(0).slice(0, Math.max(0, decimals));
                 }
             }
-            if (typeof s === 'string') {
-                s = s.replace(/^(-?\d+)((\.\d*)?)$/, function(v1, v2, v3) {
-                    return v2.replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,') + (v3.slice(0, _this.settings.decimal + 1) );
+            if (typeof s === 'string' ) {
+                s = s.replace(/^(-?\d+)((\.\d*)?)$/, function (v1, v2, v3) {
+                    if(_this.settings.formatThousand){
+                        return v2.replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,') + (v3.slice(0, _this.settings.decimal + 1));
+                    }else{
+                        return v2 + (v3.slice(0, _this.settings.decimal + 1));
+                    }
                 });
             }
             return s.replace(/^\./, "0.")
